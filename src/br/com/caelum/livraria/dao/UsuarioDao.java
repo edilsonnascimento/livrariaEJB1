2e776c8d3bbfa8348fd3bc0f9a5c15cbdc374223
Edilson do Nascimento <edilsonjoper@gmail.com>
2019-02-12 16:34:33 -0200

    01 - Introdução aos Enterprise Java Beans

---
+++ src/br/com/caelum/livraria/dao/UsuarioDao.java
@@ -0,0 +1,15 @@
+package br.com.caelum.livraria.dao;
+
+import br.com.caelum.livraria.modelo.Usuario;
+import javax.ejb.Stateless;
+
+@Stateless
+public class UsuarioDao {
+
+    private Banco banco = new Banco();
+
+    public Usuario buscaPeloLogin(String login) {
+        return this.banco.buscaPeloNome(login);
+    }
+
+}
